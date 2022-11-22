34bcb94c6cbd6090def38d7f4bcb4932a6137a81
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-22 13:22:02 +0530

    expense-tracker task-19

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,38 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from './ProductItem'
+import classes from './Products.module.css'
+
+const DUMMY_PRODUTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My first Book here',
+    description: 'The first book',
+  },
+  {
+    id: 'p2',
+    price: 6,
+    title: 'My second Book',
+    description: 'The second book',
+  },
+]
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
