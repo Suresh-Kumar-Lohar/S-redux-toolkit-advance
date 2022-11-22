286f4966ad7aa92c0f23af3e87c401c7aa2667d4
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-22 17:03:02 +0530

    expense-tracker task-21

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -15,7 +15,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData()
-      dispatch(cartActions.replaceCart(cartData))
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      )
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -41,7 +46,10 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const res = await fetch('https://money-f2b15.firebaseio.com/cart.json', {
         method: 'PUT',
-        body: JSON.stringify(cart),
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
       })
 
       if (!res.ok) {
